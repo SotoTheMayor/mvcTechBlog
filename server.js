c7984eb0cd50245e0ce49fae0ea6d1534a1b08fa
Soto <quickfire25@yahoo.com>
2023-03-17 13:51:40 -0500

    structure building

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const session = require ('express-session');
+const routes = require ('./controllers');
+const exphbs = require ('express-handlebars');
+const hbs = exphbs.create({});
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
