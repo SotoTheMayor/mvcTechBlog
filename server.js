6178b0b0a2744e2642868ec1e45bb5bae8769411
Soto <quickfire25@yahoo.com>
2023-03-18 16:00:33 -0500

    stylization added and some debugging to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-// const path = require('path');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
@@ -28,7 +28,12 @@ const sess = {
 
 app.use(session(sess));
 app.use(routes);
+// app.use(express.json);
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`)
+sequelize.sync({ force:false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`listening on http://localhost:${PORT}`)
+    })
 })
