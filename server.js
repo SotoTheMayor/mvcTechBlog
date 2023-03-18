11b9fe678ff757534de88e88ccd8e2dbce762af0
Soto <quickfire25@yahoo.com>
2023-03-18 12:50:49 -0500

    routes connecting to localhost

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const session = require ('express-session');
-const routes = require ('./controllers');
-const exphbs = require ('express-handlebars');
+// const session = require('express-session');
+const routes = require('./controllers');
+// const path = require('path');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`)
+})
