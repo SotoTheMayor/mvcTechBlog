7c732f43568d6407f6c7c1f18f57f0c6468ba0a9
Soto <quickfire25@yahoo.com>
2023-03-18 13:57:43 -0500

    login and logout handlers created

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const routes = require('./controllers');
 // const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+
+const sess = {
+    secret: 'You dont know this',
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
 
 app.use(routes);
+app.use(session(sess));
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`)
