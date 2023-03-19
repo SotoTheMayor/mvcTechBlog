6ed03b22a84eb47e100607ddefb9c32450da00f5
Soto <quickfire25@yahoo.com>
2023-03-19 16:34:56 -0500

    login names now displaying on each screen

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { Post, User } = require('../models')
 
 router.get('/', async (req, res) => {
+    if (req.session.loggedIn) {
+    const loggedUser = await User.findOne({
+        where: { id: req.session.user_id },
+        attributes: { exclude: ['password']}
+    })
     try {
         const postData = await Post.findAll({
             include: [
@@ -13,8 +18,28 @@ router.get('/', async (req, res) => {
         });
         const posts = postData.map((post) =>
             post.get({ plain:true })
-
-        
+        );
+        res.render('home', {
+            posts,
+            username: loggedUser.username,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+} else {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = postData.map((post) =>
+            post.get({ plain:true })
         );
         res.render('home', {
             posts,
@@ -23,7 +48,9 @@ router.get('/', async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
-    }
+    } 
+}
 });
+
 
 module.exports = router;
