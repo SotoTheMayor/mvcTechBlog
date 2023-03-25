1f09f139392525c05d7701efbe59c1183478ef15
Soto <quickfire25@yahoo.com>
2023-03-25 11:03:36 -0500

    added framework for dashboard and comments, created comment model

---
+++ models/comment.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                max: [50],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Post',
+                key: 'id',
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+    module.exports = Comment; 
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const Post = require('./post');
+const Comment = require('./comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
@@ -9,4 +10,16 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Post, Comment };
